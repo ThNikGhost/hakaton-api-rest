b661f5859d40685243a035dc3b3f020751e4e77b
ThGhost <orlov.icloud@mail.ru>
2023-02-24 15:03:12 +0600

    Update repo ( всё снёс по новой)

--- Project/local.py
+++ Project/local.py
@@ -1,37 +1,6 @@
 import requests
 
-while True:
-    text = input('Что хотите?')
-    if text == 'qq':
-        break
-    elif text == 'get':
-        while True:
-                text = input()
-                if text == 'qq':
-                    break
-                res = requests.get(f"http://127.0.0.1:3000/api/city-info/{text}")
-                print(res.json())
-    elif text == 'delete':
-        while True:
-                text = input()
-                if text == 'qq':
-                    break
-                res = requests.delete(f"http://127.0.0.1:3000/api/city-info/{text}")
-                print(res.json())
-    elif text == 'post':
-        while True:
-                id = input('Введите id города:')
-                name = input('Введите название города:')
-                if name == 'qq':
-                    break
-                res = requests.post(f"http://127.0.0.1:3000/api/city-info/{id}", json={'name': name, 'id': id})
-                print(res.json())
-    elif text == 'put':
-        while True:
-                id = input('Введите id города, который вы хотите изменить:')
-                name = input('Введите новое название города:')
-                if name == 'qq':
-                    break
-                res = requests.put(f"http://127.0.0.1:3000/api/city-info/{id}", json={'name': name, 'id': id})
-                print(res.json())
+
+res = requests.get(f"http://127.0.0.1:3000/api/main")
+print(res.json())
 
--- Project/test.py
+++ Project/test.py
@@ -1,12 +1,1 @@
-import os.path
-from work_db import *
-conn = sqlite3.connect('db.sqlite3')
-cur = conn.cursor()
-city_id = 15
-city_name = 'niko'
-cur.execute(f"""INSERT INTO Cities (city_id,city_name)
-                   VALUES (
-                       '{city_id}',
-                       '{city_name}'
-                   )
-                        """)
+print(type('t'))
