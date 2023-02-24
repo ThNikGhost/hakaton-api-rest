66801be9e48fa52b5ee20bab418d5d32ad2215af
ThGhost <orlov.icloud@mail.ru>
2023-02-24 16:39:46 +0600

    добавил вывод погоды в городе

--- Project/main.py
+++ Project/main.py
@@ -1,22 +1,33 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask import render_template
+import requests
 
 app = Flask(__name__)
 api = Api()
-
-some_dict = {
-    'name': 'Omsk',
-    'clouds': 56,
-    'temp': 0.97,
-}
+url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=3fd71f50684a58f9c310b7a4e5df243f'
 
 class Main(Resource):
+    def get(self, city):
+        res = requests.get(url.format(city)).json()
+        try:
+            city_info = {
+                'city': res['name'],                       # Название города 
+                'temp': res['main']['temp'],               # Температура в городе
+                'temp_feels': res['main']['feels_like'],   # Температура по ощущениям
+                'wind_speed': res['wind']['speed'],        # Скорость ветра (вроде м/c)
+            }
+        except KeyError:
+            return 'Incorect name'    #Если неверное имя города, то возвращает эту строку 
+        return city_info
 
-    def get(self):
-        return some_dict
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-api.add_resource(Main, '/api/main')
+
+api.add_resource(Main, '/api/main/<string:city>')
 api.init_app(app)
 
 if __name__ == '__main__':
